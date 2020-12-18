d9ed35ab71b5867bbd7ad3b0ddbc07ad92afdeb2
z1kary <vicburet@orange.fr>
2020-12-18 11:50:17 +0100

    Add friends to users

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_stock', to: 'stocks#search'
+  get 'friends', to: 'users#friends'
+  get 'search_friend', to: 'users#search'
 end
